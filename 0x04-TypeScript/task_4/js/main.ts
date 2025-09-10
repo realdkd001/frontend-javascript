e841056e6d9bb2efc3fe0b3d78e7911bac2b1352
Dzrekey001 <dkdzrekey001@gmail.com>
2025-09-10 16:00:10 +0000

    fix: 200

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,33 +1,29 @@
-import Cpp from "./subjects/Cpp";
-import Java from "./subjects/Java";
-import React from "./subjects/React";
-import { Teacher } from "./subjects/Teacher";
+/// <reference path="./subjects/Subject.ts" />
+/// <reference path="./subjects/Java.ts" />
+/// <reference path="./subjects/Cpp.ts" />
+/// <reference path="./subjects/React.ts" />
+/// <reference path="./subjects/Teacher.ts" />
 
-// create and export constants
-export const cpp = new Cpp();
-export const java = new Java();
-export const react = new React();
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
 
-// create and export teacher
-export const cTeacher: Teacher = {
+export const cTeacher: Subjects.Teacher = {
   firstName: "John",
   lastName: "Doe",
   experienceTeachingC: 10,
 };
 
-// Cpp subject
 console.log("C++");
 cpp.setTeacher(cTeacher);
 console.log(cpp.getRequirements());
 console.log(cpp.getAvailableTeacher());
 
-// Java subject
 console.log("Java");
 java.setTeacher(cTeacher);
 console.log(java.getRequirements());
 console.log(java.getAvailableTeacher());
 
-// React subject
 console.log("React");
 react.setTeacher(cTeacher);
 console.log(react.getRequirements());
