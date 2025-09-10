528d73e5b04b6599fbe9190d25ad6808969acc1b
Dzrekey001 <dkdzrekey001@gmail.com>
2025-09-10 15:48:57 +0000

    fix:100

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,14 +1,10 @@
 /// <reference path="./subjects/Subject.ts" />
-/// <reference path="./subjects/Java.ts" />
-/// <reference path="./subjects/Cpp.ts" />
-/// <reference path="./subjects/Teacher.ts" />
-
 
 export const cpp = new Subjects.Cpp();
 export const java = new Subjects.Java();
 export const react = new Subjects.React();
 
-export const cTeacher:Subjects.Teacher = {firstName: "John", lastName: "Doe", experienceTeachingC: 10};
+export const cTeacher: Subjects.Teacher = {firstName: "John", lastName: "Doe", experienceTeachingC: 10};
 
 console.log("C++");
 cpp.setTeacher(cTeacher);
