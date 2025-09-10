c3e41d8ce4d66f92b0d3f289d6f03fef1f1b223d
Dzrekey001 <dkdzrekey001@gmail.com>
2025-09-10 15:00:52 +0000

    Update task_4/js/main.ts

---
+++ 0x04-TypeScript/task_4/js/subjects/main.ts
@@ -0,0 +1,26 @@
+/// <reference path="./Subject.ts" />
+/// <reference path="./Java.ts" />
+/// <reference path="./Cpp.ts" />
+/// <reference path="./Teacher.ts" />
+
+
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
+
+export const cTeacher:Subjects.Teacher = {firstName: "John", lastName: "Doe", experienceTeachingC: 10};
+
+console.log("C++");
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+console.log("Java");
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+console.log("React");
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
